ad4e5dc7b4f1a8dc11ba3b69205d70e1b90ca4bf
FinnE145 <finn.emmerson@renertschool.ca>
2024-08-10 09:31:39 -0600

    Maint: Copy and edit FEWebsite code

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/restart", methods=["GET", "POST"])
+def restart():
+    print(request.form)
+    return "Nothing happened"
+
+if __name__ == "__main__":
+    app.run()
